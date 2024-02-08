1c0e69cde22da065b17d42677665c60a3269b40f
Vitalii Tokioi <v.tokioy@gamil.com>
2024-02-08 10:50:10 +0200

    Write data to files

--- data-engineering-on-aws/itv-ghactivity/app.py
+++ data-engineering-on-aws/itv-ghactivity/app.py
@@ -2,6 +2,7 @@ import os
 from util import get_spark_session
 from read import from_files
 from process import transform
+from write import to_files
 
 
 def main():
@@ -9,12 +10,13 @@ def main():
     src_dir = os.environ.get('SRC_DIR')   # /home/vtokioy/Projects/Internal/bootcamp/data-engineering-on-aws/itv-ghactivity/data/itv-github/landing/ghactivity
     src_file_pattern = f'{os.environ.get("SRC_FILE_PATTERN")}-*'  # 2024-02-07
     src_file_format = os.environ.get("SRC_FILE_FORMAT")           # json
+    tgt_dir = os.environ.get('TGT_DIR')  # /home/vtokioy/Projects/Internal/bootcamp/data-engineering-on-aws/itv-ghactivity/data/itv-github/landing/ghactivity
+    tgt_file_format = os.environ.get("TGT_FILE_FORMAT")  # json
 
     spark = get_spark_session(env, 'ITV GitHub activity  - Getting Started')
     df = from_files(spark, src_dir, src_file_pattern, src_file_format)
     df_transformed = transform(df)
-    df_transformed.printSchema()
-    df_transformed.select('repo.*', 'created_at', 'year', 'month', 'day').show()
+    to_files(df_transformed, tgt_dir, tgt_file_format)
 
 
 if __name__ == '__main__':
--- data-engineering-on-aws/itv-ghactivity/util.py
+++ data-engineering-on-aws/itv-ghactivity/util.py
@@ -8,4 +8,12 @@ def get_spark_session(env, app_name):
             master('local'). \
             appName(app_name). \
             getOrCreate()
-        return spark
+        spark.conf.set("spark.sql.debug.maxToStringFields", 100000)
+    elif env == 'PROD':
+        spark = SparkSession. \
+            builder. \
+            master('yarn'). \
+            appName(app_name). \
+            getOrCreate()
+        spark.conf.set("spark.sql.debug.maxToStringFields", 100000)
+    return spark
