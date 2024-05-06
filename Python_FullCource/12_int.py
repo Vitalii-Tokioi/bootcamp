ec87eab2a0e69ec349e9fee5df3759d05b750301
Vitalii Tokioi <v.tokioy@gamil.com>
2024-05-06 14:17:36 +0300

    [python-full-cource] 12 - Numbers

---
+++ Python_FullCource/12_int.py
@@ -0,0 +1,10 @@
+base = 3
+power = 5
+print(pow(base, power))
+
+one_million = 1_000_000
+print(one_million)
+
+avg_price = 18.55
+print(int(avg_price))  #return 18
+print(round(avg_price)) #return 19
